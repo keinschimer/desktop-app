2474eacaad1d936ad1c3a25c2b6bfc71b21ec493
keinschimer <keinschimermax@gmail.com>
2017-09-27 18:12:43 +0200

    Changes

--- files.js
+++ files.js
@@ -5,21 +5,5 @@ Item: function(path, name, score, status, more) {
   this.score = score;
   this.status = status;
   this.more = more;
-
-  this.addPath = function (path) {
-    this.path = path;
-  }
-  this.addName = function (name) {
-    this.name = name;
-  }
-  this.addScoer = function (scoer) {
-    this.scoer = scoer;
-  }
-  this.addStatus = function (status) {
-    this.status = status;
-  }
-  this.addMore = function (more) {
-    this.more = more;
-  }
 }
 }
--- renderer.js
+++ renderer.js
@@ -8,7 +8,7 @@ const {
 const db = new Datastore({
   filename: './file.db',
   autoload: true
-})
+});
 var myFile
 var isNameAvaliable = 0
 var items = []
@@ -35,27 +35,21 @@ function openFilePicker() {
         }
       }
       if (isNameAvaliable == 0) {
-        // console.log(myFile[i]);
-        let output = '<tr><td class="tdtitle"><p class="title">' + fileName + '</p></td><td class="tdrating"><select class="rating"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option></select></td><td class="tdstatus"><select class="status"><option value="fin">Finished</option><option value="wat">Watching</option><option value="ptw">Plan to Watch</option></select></td><td class="tdmore"><textarea class="more" rows="1" cols="10"></textarea></td><td class="tdpath"><p class="path">' + myFile[i] + '</p></td></tr>'
-        document.getElementById('table').innerHTML += output
+        generateRow(fileName, myFile, i)
       }
     }
   }
 }
 
-function onClickFunction() {
-
+function generateRow(fileName, myFile, i) {
+  var output = '<tr><td class="tdtitle"><p class="title">' + fileName + '</p></td><td class="tdrating"><select class="rating"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option></select></td><td class="tdstatus"><select class="status"><option value="fin">Finished</option><option value="wat">Watching</option><option value="ptw">Plan to Watch</option></select></td><td class="tdmore"><textarea class="more" rows="1" cols="10"></textarea></td><td class="tdpath"><p class="path">' + myFile[i] + '</p></td></tr>'
+  document.getElementById('table').innerHTML += output
 }
 
-function pls_Work() {
-  for (var i = 0; i < document.getElementsByClassName('rating').length; i++) {
-    document.getElementsByClassName('rating')[i].onclick = onClickFunction;
-    console.log(i);
-  }
-}
-
-function saveDB() { //items is gonna be the array of the files
+function saveDB() {
+  // IDEA: Overrite already exsiteing items
   items = []
+  // var id = document.getElementsByClassName('id')
   var path = document.getElementsByClassName('path')
   var title = document.getElementsByClassName('title')
   var rating = document.getElementsByClassName('rating')
@@ -70,9 +64,17 @@ function saveDB() { //items is gonna be the array of the files
 }
 
 function loadDB() {
-  console.log("dummy-button");
+  db.find({}, function(err, docs) {
+    console.log(docs);
+    for (var i = 0; i < docs.length; i++) {
+      generateRow(docs[i].name, docs[i].path)
+      document.getElementsByClassName('rating')[i].value = docs[i].score
+      document.getElementsByClassName('status')[i].value = docs[i].status
+      document.getElementsByClassName('more')[i].value = docs[i].more
+    }
+  })
 }
 
-openBtn.addEventListener('click', openFilePicker)
-saveBtn.addEventListener('click', saveDB)
-loadBtn.addEventListener('click', loadDB)
+openBtn.onclick = openFilePicker
+saveBtn.onclick = saveDB
+loadBtn.onclick = loadDB
