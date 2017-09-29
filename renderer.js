84cd2ba98176f52b0542db12098e5769d831aa9f
keinschimer <keinschimermax@gmail.com>
2017-09-30 00:43:24 +0200

    Most Functions do something

--- renderer.js
+++ renderer.js
@@ -2,13 +2,17 @@ const Item = require('./files.js');
 const path = require('path');
 const fs = require('fs');
 const Datastore = require('nedb')
-const {dialog} = require('electron').remote
-const db = new Datastore({filename: './file.db', autoload: true});
+const {
+  dialog
+} = require('electron').remote
+const db = new Datastore({
+  filename: './file.db',
+  autoload: true
+});
 var isNameAvaliable = 0
 var myFile
-var items = []
 
-function openFilePicker() { //Kinda Works
+function openFilePicker() { // IDEA: look for Path not Name!! //Kinda Works
   myFile = dialog.showOpenDialog({
     properties: [
       'openFile', // on Windows/Linux only one
@@ -30,53 +34,39 @@ function openFilePicker() { //Kinda Works
         }
       }
       if (isNameAvaliable == 0) {
-        generateRow(fileName, myFile, i)
+        generateRow(fileName, myFile[i])
       }
     }
   }
 }
 
-function saveDB() {
+function saveDB() {// needs testing when loading is finished
   // IDEA: Overrite already exsiteing items
   var isInDb
-  items = []
+  let items = []
   // var id = document.getElementsByClassName('id')
   var pfad = document.getElementsByClassName('path')
   var title = document.getElementsByClassName('title')
   var rating = document.getElementsByClassName('rating')
   var status = document.getElementsByClassName('status')
   var more = document.getElementsByClassName('more')
-  for (var i = 0; i < title.length; i++) {
-    db.find({path: pfad.innerText}, function (err, docs) {
-      console.log(docs);
+  for (let i = 0; i < title.length; i++) {
+    db.remove({path: pfad[i].innerText}, function(err, numRemoved) {
+      console.log(numRemoved);
+      let item = new Item.Item(pfad[i].innerText, title[i].innerText, rating[i].value, status[i].value, more[i].value)
+      items.push(item);
+      console.log(item);
     })
-    var item = new Item.Item(
-      pfad[i].innerText,
-      title[i].innerText,
-      rating[i].value,
-      status[i].value,
-      more[i].value)
-    items.push(item);
-    console.log(item);
   }
-  console.log(items);
-  db.find({}, function(err, docs) {
-    isInDb = docs
-    console.log(isInDb);
-    // if (isInDb != undefined) {
-    // } else {
-      // db.insert(items, function(err, doc) {})
-    // }
-  })
-  // console.log(isInDb);
-
+  db.insert(items, function(err, doc) {console.log(items);console.log('Saved!');})
 }
 
-function loadDB() { // Works good enough
+function loadDB() { // IDEA: replace the alredy existing table {ez}
   db.find({}, function(err, docs) {
-    console.log(docs);
+    // console.log(docs);
     for (var i = 0; i < docs.length; i++) {
       generateRow(docs[i].name, docs[i].path)
+      console.log(docs[i].score);
       document.getElementsByClassName('rating')[i].value = docs[i].score
       document.getElementsByClassName('status')[i].value = docs[i].status
       document.getElementsByClassName('more')[i].value = docs[i].more
@@ -84,8 +74,8 @@ function loadDB() { // Works good enough
   })
 }
 
-function generateRow(fileName, myFile, i) {
-  var output = '<tr><td class="tdtitle"><p class="title">' + fileName + '</p></td><td class="tdrating"><select class="rating"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option></select></td><td class="tdstatus"><select class="status"><option value="fin">Finished</option><option value="wat">Watching</option><option value="ptw">Plan to Watch</option></select></td><td class="tdmore"><textarea class="more" rows="1" cols="10"></textarea></td><td class="tdpath"><p class="path">' + myFile[i] + '</p></td></tr>'
+function generateRow(fileName, myFile){ // IDEA: <option value="-">-</option>(load) //was to lazy to write that again for load
+  let output = '<tr><td class="tdtitle"><p class="title">' + fileName + '</p></td><td class="tdrating"><select class="rating"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option></select></td><td class="tdstatus"><select class="status"><option value="fin">Finished</option><option value="wat">Watching</option><option value="ptw">Plan to Watch</option></select></td><td class="tdmore"><textarea class="more" rows="1" cols="10"></textarea></td><td class="tdpath"><p class="path">' + myFile + '</p></td></tr>'
   document.getElementById('table').innerHTML += output
 }
 
