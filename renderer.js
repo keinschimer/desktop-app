197d1fbd29d6af9a42ba43b37ae561c544bb492f
keinschimer <keinschimermax@gmail.com>
2017-10-03 17:09:38 +0200

    Changes

--- renderer.js
+++ renderer.js
@@ -9,11 +9,9 @@ const db = new Datastore({
   filename: './file.db',
   autoload: true
 });
-var isNameAvaliable = 0
-var myFile
 
-function openFilePicker() { // IDEA: look for Path not Name!! //Kinda Works
-  myFile = dialog.showOpenDialog({
+function openFilePicker() { // TODO: look for Path not Name!! / only open vidoe types //Kinda Works
+  var myFile = dialog.showOpenDialog({
     properties: [
       'openFile', // on Windows/Linux only one
       'promtToCreate',
@@ -23,7 +21,7 @@ function openFilePicker() { // IDEA: look for Path not Name!! //Kinda Works
   if (myFile != undefined) {
     var titles = document.getElementsByClassName('title')
     for (let i = 0; i < myFile.length; i++) {
-      isNameAvaliable = 0;
+      var isNameAvaliable = 0;
       var fileExt = path.extname(myFile[i])
       var fileName = path.basename(myFile[i], fileExt)
       for (let i = 0; i < titles.length; i++) {
@@ -40,7 +38,7 @@ function openFilePicker() { // IDEA: look for Path not Name!! //Kinda Works
   }
 }
 
-function saveDB() {// needs testing when loading is finished
+function saveDB() { // needs testing when loading is finished
   // IDEA: Overrite already exsiteing items
   var isInDb
   let items = []
@@ -51,33 +49,43 @@ function saveDB() {// needs testing when loading is finished
   var status = document.getElementsByClassName('status')
   var more = document.getElementsByClassName('more')
   for (let i = 0; i < title.length; i++) {
-    db.remove({path: pfad[i].innerText}, function(err, numRemoved) {
+    db.remove({
+      path: pfad[i].innerText
+    }, function(err, numRemoved) {
       console.log(numRemoved);
       let item = new Item.Item(pfad[i].innerText, title[i].innerText, rating[i].value, status[i].value, more[i].value)
       items.push(item);
       console.log(item);
     })
   }
-  db.insert(items, function(err, doc) {console.log(items);console.log('Saved!');})
+  db.insert(items, function(err, doc) {
+    console.log(items);
+    console.log('Saved!');
+  })
 }
 
-function loadDB() { // IDEA: replace the alredy existing table {ez}
-db.find({}, function(err, docs) {
-  console.log(docs);
-  for (var i = 0; i < docs.length; i++) {
-    generateRow(docs[i].name, docs[i].path)
-    console.log(document.getElementsByClassName('rating')[i].value);
-    document.getElementsByClassName('rating')[i].value = docs[i].score
-    console.log(document.getElementsByClassName('rating')[i].value);
-    document.getElementsByClassName('status')[i].value = docs[i].status
-    document.getElementsByClassName('more')[i].value = docs[i].more
-  }
-})
+function loadDB() { // TODO: replace the alredy existing table {ez}
+  // TODO: fix score
+  db.find({}, function(err, docs) {
+    console.log(err);
+    for (let i = 0; i < docs.length; i++) {
+      generateRow(docs[i].name, docs[i].path, docs[i].score, i)
+      console.log(document.getElementsByClassName('rating')[i].value);
+      document.getElementsByClassName('rating')[i].id = "test"+i
+      document.getElementsByClassName('rating')[i].value = docs[i].score
+      // console.log(document.getElementsByClassName('rating')[i].value);
+      document.getElementsByClassName('status')[i].value = docs[i].status
+      document.getElementsByClassName('more')[i].value = docs[i].more
+    }
+  })
 }
 
-function generateRow(fileName, myFile){ // IDEA: <option value="-">-</option>(load) //was to lazy to write that again for load
-  let output = '<tr><td class="tdtitle"><p class="title">' + fileName + '</p></td><td class="tdrating"><select class="rating"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option></select></td><td class="tdstatus"><select class="status"><option value="fin">Finished</option><option value="wat">Watching</option><option value="ptw">Plan to Watch</option></select></td><td class="tdmore"><textarea class="more" rows="1" cols="10"></textarea></td><td class="tdpath"><p class="path">' + myFile + '</p></td></tr>'
+function generateRow(fileName, myFile, value, i) {//was to lazy to write that again for load
+  let output = '<tr><td class="tdtitle"><p class="title">' + fileName + '</p></td><td class="tdrating"><select class="rating"><option value="-">-</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option></select></td><td class="tdstatus"><select class="status"><option value="fin">Finished</option><option value="wat">Watching</option><option value="ptw">Plan to Watch</option></select></td><td class="tdmore"><textarea class="more" rows="1" cols="10"></textarea></td><td class="tdpath"><p class="path">' + myFile + '</p></td></tr>'
   document.getElementById('table').innerHTML += output
+  if (value != undefined) {
+    document.getElementsByClassName('rating')[i].value = value
+  }
 }
 
 // IDEA: put that shet in the toolbar
